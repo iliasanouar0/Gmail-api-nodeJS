650e9c787bbc9fa7fee82f93d0603f52ac70cc15
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-26 12:01:47 +0000

    adding functions

--- server/app.js
+++ server/app.js
@@ -7,19 +7,21 @@ const app = express();
 const routes = require("./routes");
 app.use('/api', routes);
 
+// app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // disabled for security on local
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 app.listen(process.env.PORT, () => {
   console.log("listening on port " + process.env.PORT);
 });
 
+
+
 app.get("/", async (req, res) => {
-  fs.readFile('./client/index.html', (e, d) => {
-    if (e) {
-      res.status(500).send(e)
-      return e
-    }
-    console.log(d);
-    res.send(d.toString());
-  })
+  res.send('<div>test</div>');
 });
 
 app.get("/lists", async (req, res) => {
--- server/client/index.js
+++ server/client/index.js
@@ -1,1 +1,11 @@
-console.log('test');
+const root = document.getElementById('root')
+let REDIRECT_URI = location.href.split('/')[location.href.split('/').length - 1]
+console.log(REDIRECT_URI);
+switch (REDIRECT_URI) {
+    case '':
+        fetch("http://localhost:8000/")
+        break;
+    default:
+        console.log("none");
+        break;
+}
