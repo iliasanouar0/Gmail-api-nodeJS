98f95e18f43b890d19202e652b8dded391d9a36f
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-26 12:05:30 +0000

    adding functions

--- server/client/index.js
+++ server/client/index.js
@@ -3,7 +3,11 @@ let REDIRECT_URI = location.href.split('/')[location.href.split('/').length - 1]
 console.log(REDIRECT_URI);
 switch (REDIRECT_URI) {
     case '':
-        fetch("http://localhost:8000/")
+        fetch("http://localhost:8000/", { method: "GET" }).then(res => {
+            return res.text()
+        }).then(data => {
+            root.innerHTML = data
+        })
         break;
     default:
         console.log("none");
