59f903401983b46f659bc05a92923ea4f1e519af
iliasanouar0 <iliasanouar0@gmail.com>
2024-01-08 10:34:05 +0000

    adding functions

--- server/controllers.js
+++ server/controllers.js
@@ -94,6 +94,7 @@ async function getUser(req, res) {
         process.env.CLIENT_SECRET,
         process.env.REDIRECT_URI
     );
+    oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
     try {
         const url = `https://gmail.googleapis.com/gmail/v1/users/${req.params.email}/profile`;
         const { token } = await oAuth2Client.getAccessToken();
